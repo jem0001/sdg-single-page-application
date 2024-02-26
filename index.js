7b34982bf7c70998d6490880d1df21199063bbdf
Jemuel <salcedojemuel25@gmail.com>
2024-02-26 11:43:03 +0800

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // TOGGLE MENU
+  const toggleButton = document.querySelector("#menu");
+  const sidebar = document.querySelector("#sidebar");
+
+  toggleButton.addEventListener("click", function () {
+    console.log(sidebar.style.right);
+    if (sidebar.style.right !== "0px") {
+      sidebar.style.right = 0;
+    } else {
+      sidebar.style.right = "-100%";
+    }
+  });
+
+  // ROTATING SLIDER
   let degrees = 0;
   const nextDesc = document.querySelector(".swiper-button-next");
   const redDiv = document.querySelector(".bg-red-400");
