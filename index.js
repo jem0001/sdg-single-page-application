58bd1b21bb42922d697d0e812fe543219819c585
JohnPatrickArago <148320966+JohnPatrickArago@users.noreply.github.com>
2024-02-27 04:34:26 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // ROTATING SLIDER
   let degrees = 0;
   const nextDesc = document.querySelector(".swiper-button-next");
-  const redDiv = document.querySelector(".bg-red-400");
+  const redDiv = document.querySelector("#sirkol");
   redDiv.addEventListener("click", function () {
-    degrees = degrees + 90;
+    degrees = degrees + 21.1764;
     redDiv.style.transform = `rotate(${degrees}deg)`;
     nextDesc.click();
   });
